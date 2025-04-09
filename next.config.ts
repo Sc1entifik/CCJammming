d7c91df2d79eb9fc5cb7b85778b55328e5d7af8b
Scientifik <scientifik1@protonmail.com>
2025-04-09 01:24:45 -0400

    Trying different deployment type.

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,6 @@ const nextConfig: NextConfig = {
 			allowedOrigins: ["localhost:3000","192.168.2.4:3000","http://localhost:3000","https://localhost:3000", "https://192.168.2.4:3000"],
 		},
 	},
-	output: "standalone",
 	images: {
 		remotePatterns: [
 			{
