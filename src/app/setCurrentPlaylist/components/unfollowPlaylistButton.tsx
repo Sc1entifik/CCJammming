00563a095a9e72f92d1617e5e1f73df116b0f571
Scientifik <scientifik1@protonmail.com>
2025-05-02 13:09:01 -0400

    Tweaked header color a little. Changed unfillow current Playlist button color

--- src/app/setCurrentPlaylist/components/unfollowPlaylistButton.tsx
+++ src/app/setCurrentPlaylist/components/unfollowPlaylistButton.tsx
@@ -9,7 +9,7 @@ export default function UnfollowPlaylistButton() {
 		<div className="flex flex-col items-center max-w-96 text-textColor mx-4">
 			<h2>Unfollowing a playlist is the closest thing Spotify has to deleting a playlist from your account. To refollow a playlist after it has been unfollowed please <Link className="italic font-bold" href={SpotifyEndpoints.SPOTIFY_RECOVER_PLAYLISTS}>follow the instructions from Spotify found in this link.</Link></h2>
 			<br/>
-			<button className="font-mono px-4 py-2 font-bold bg-sky-500 hover:bg-sky-900 rounded" onClick={unfollowCurrentPlaylist}>UNFOLLOW CURRENT PLAYLIST</button>
+			<button className="font-mono px-4 py-2 font-bold text-black bg-lime-300 hover:bg-amber-900 rounded" onClick={unfollowCurrentPlaylist}>UNFOLLOW CURRENT PLAYLIST</button>
 		</div>
 	);
 }
