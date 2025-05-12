617058dabb8976db7ddf979f6595b973c6ed8d8e
Scientifik <scientifik1@protonmail.com>
2025-05-12 14:47:02 -0400

    Added Logos to all songs from collapsableAlbum element.

--- src/app/components/mainAppDisplay/searchBarElements/albums/albumGrid/albumCollapse/albumTracks.tsx
+++ src/app/components/mainAppDisplay/searchBarElements/albums/albumGrid/albumCollapse/albumTracks.tsx
@@ -1,6 +1,8 @@
 import { SpotifyImage } from "@/utils/fetchInterfaces"
 import Image from "next/image"
 import AddTracksToPlaylist from "../../../commonElements/addTracksToPlaylist"
+import Link from "next/link"
+import SpotifyLogo from "@/components/spotifyLogo/page"
 
 interface SimplifiedTrackObject {
 	name: string
@@ -18,6 +20,9 @@ const imageStyle = {
 export default function AlbumTrack({track, albumCover}: {track: SimplifiedTrackObject, albumCover: SpotifyImage}) {
 	return (
 		<div className="grid grid-cols-subgrid justify-items-start"> 
+			<Link href={track.uri} target="_blank">
+				<SpotifyLogo remSize={5}/>
+			</Link>
 			<AddTracksToPlaylist trackUris={[track.uri]}>
 				<Image alt="tiny album cover" src={albumCover.url} height={5000} width={5000} style={imageStyle}/>
 			</AddTracksToPlaylist>
