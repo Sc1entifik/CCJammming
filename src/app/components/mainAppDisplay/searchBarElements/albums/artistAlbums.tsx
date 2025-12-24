6789599911e261ced65659d4f87dd755f536b848
Scientifik <scientifik1@protonmail.com>
2025-12-23 21:18:28 -0500

    Committing to not lose changes on search bar components while I troubleshoot issues

--- src/app/components/mainAppDisplay/searchBarElements/albums/artistAlbums.tsx
+++ src/app/components/mainAppDisplay/searchBarElements/albums/artistAlbums.tsx
@@ -7,10 +7,12 @@ export default function ArtistAlbums({artistName}: {artistName: string}) {
 	return (
 		<div className="flex flex-col items-center font-tropiLand gap-4 max-w-160 max-h-[84dvh]">
 			<ArtistNameAndImage artistName={artistName}/>
-			<h3 className="text-textColor">Scroll To See All Albums</h3>
-			<h3 className="text-textColor">Click Album To Expand</h3>
-			<h3 className="text-textColor">Click Song To Add To Playlist</h3>
-			<h3 className="text-textColor">Click Spotify Tag To Lisen On Spotify</h3>
+			<div className="flex flex-col items-start">
+				<h3 className="text-textColor">Scroll To See All Albums</h3>
+				<h3 className="text-textColor">Click Album To Expand</h3>
+				<h3 className="text-textColor">Click Song To Add To Playlist</h3>
+				<h3 className="text-textColor">Click Spotify Tag To Lisen On Spotify</h3>
+			</div>
 			<AlbumGrid artistName={artistName} fetchType="artist name"/>
 		</div>
 	);
--- src/app/components/mainAppDisplay/searchBarElements/artistTopTracks/artistTopTracks.tsx
+++ src/app/components/mainAppDisplay/searchBarElements/artistTopTracks/artistTopTracks.tsx
@@ -6,11 +6,13 @@ export default function ArtistTopTracks({artistName}: {artistName: string}) {
 
 	return (
 		<div className="ml-2 mr-2 flex flex-col xl:flex-row font-tropiLand xl:gap-20">
-			<div>
+			<div className="flex flex-col items-center">
 				<ArtistNameAndImage artistName={artistName}/>
-				<p className="mt-4 text-textColor tracking-wide">Scroll To See All Songs</p>
-				<p className="text-textColor tracking-wide">Click Song To Add To Playlist</p>
-				<p className="mb-4 text-textColor tracking-wide">Click Spotify Tag To Play On Spotify</p>
+				<div className="flex flex-col items-start">
+					<p className="mt-4 text-textColor tracking-wide">Scroll To See All Songs</p>
+					<p className="text-textColor tracking-wide">Click Song To Add To Playlist</p>
+					<p className="mb-4 text-textColor tracking-wide">Click Spotify Tag To Play On Spotify</p>
+				</div>
 			</div>
 			<SongGrid queryTerm={artistName} />
 		</div>
